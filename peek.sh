191fab3b82b7a2f2243a9d144a82f7288f801d85
mariachaort <maria.c.o.2000@gmail.com>
2023-11-02 15:42:59 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,13 +1,14 @@
 num=$2
-if [[ -z $num ]]; then num=3; fi
+if [[ -z "$num" ]]; then num=3; fi
 
-numlines=$(wc -l $1)
+numlines=$(wc -l < "$1")
 
-if [[ $numlines -ne 2*$num || $numlines -le 2*$num ]]; 
-then cat $1 
+if [[ "$numlines" -le $((2*num)) ]]; 
+then cat "$1"
 else 
-	echo "Warning: not all lines will be displayed"
-	echo $(head -n $num $1) 
-	echo "…"
-	echo $(tail -n $num $1);
+	echo "Warning: not all lines will be displayed";
+	head -n "$num" "$1";
+	echo "…";
+	tail -n "$num" "$1";
 fi
+
