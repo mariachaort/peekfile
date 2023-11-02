cb6eae6bf48e71d60d5bb10227f47b9c0fde5c67
mariachaort <maria.c.o.2000@gmail.com>
2023-11-02 15:26:08 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,3 +1,13 @@
-head -n 3 $1
-echo "..."
-tail -n $2 $1
+num=$2
+if [[ -z $num ]]; then num=3; fi
+
+numlines=$(wc -l $1)
+
+if [[ $numlines -ne 2*$num || $numlines -le 2*$num ]]; 
+then cat $1 
+else 
+	echo "Warning: not all lines will be displayed"
+	echo $(head -n $num $1) 
+	echo "…"
+	echo $(tail -n $num $1);
+fi
